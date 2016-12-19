62f31b950f1c7b7108aba9063acff7f2c48b6335
Walker Lin (MSFT) <a-walin@microsoft.com>
2016-12-19 17:13:41 +0800

    add the scripts of SQL and Stream Analytics

---
+++ AzureSQL/createsql.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA [Prod]
+CREATE TABLE [Prod].[HistoricData]
+(
+[Id] INT IDENTITY (1, 1) NOT NULL,
+[IoTHubDeviceID] NVARCHAR(128) NULL,
+[MessageID] NVARCHAR(128) NULL,
+[Light] FLOAT(10) NULL,
+[Depreciation] FLOAT(10) NULL,
+[Power] FLOAT(10) NULL,
+[Repair] FLOAT NULL,
+[RepairAlarm] INT DEFAULT ((0)) NULL,
+[LocalTime] DATETIME DEFAULT (getdate()) NULL,
+[CreatedAt] DATETIME DEFAULT (getdate()) NULL,
+PRIMARY KEY CLUSTERED ([Id] ASC)
+)
